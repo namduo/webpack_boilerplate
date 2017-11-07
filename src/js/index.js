b48825d0b13bb02931fd23edd5c7052b66cfd106
Nam Nguyen <nm.duong@outlook.com>
2017-11-07 11:14:50 +0000

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+const css = require('../scss/main.scss');
+
+console.log('hello from ./src/js/index.js');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	devServer: {
+    contentBase: './dist',
+		watchContentBase: true
+  },
+	plugins: [
+		new ExtractTextPlugin('main.css')
+	]
+}
